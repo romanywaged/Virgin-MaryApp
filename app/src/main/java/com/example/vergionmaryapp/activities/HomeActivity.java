38e95531c21545d9a09089216331dfae0baec8d2
romany <romanywaged2@gmail.com>
2021-07-31 23:28:06 +0200

    prefinal_commit

--- app/src/main/java/com/example/vergionmaryapp/activities/HomeActivity.java
+++ app/src/main/java/com/example/vergionmaryapp/activities/HomeActivity.java
@@ -1,22 +1,44 @@
 package com.example.vergionmaryapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.vergionmaryapp.R;
+import com.example.vergionmaryapp.adapters.SlidehomeAdapter;
 import com.example.vergionmaryapp.booking.showEvents.EventListViewActivity;
+import com.example.vergionmaryapp.models.PagerClass;
 import com.example.vergionmaryapp.models.SetUpDatabase;
 import com.example.vergionmaryapp.models.SharedPrefrenceModel;
 import com.example.vergionmaryapp.utils.CommonMethod;
+import com.google.android.material.tabs.TabLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity
 {
     SharedPrefrenceModel sharedPrefrenceModel;
+    private List<PagerClass> pagers;
+    private SlidehomeAdapter adapter;
+
+
+
+    @BindView(R.id.homePager)
+    ViewPager viewPager;
+
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,6 +56,26 @@ public class HomeActivity extends AppCompatActivity
             database.setup();
             sharedPrefrenceModel.SaveShared(true);
         }
+
+
+
+        pagers=new ArrayList<>();
+
+        //Slider
+        pagers.add(new PagerClass(R.drawable.vergionmary));
+        pagers.add(new PagerClass(R.drawable.vergion));
+        pagers.add(new PagerClass(R.drawable.churcch));
+        pagers.add(new PagerClass(R.drawable.churchh));
+        pagers.add(new PagerClass(R.drawable.church));
+        adapter=new SlidehomeAdapter(HomeActivity.this,pagers);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager,true);
+
+
+        Timer timer=new Timer();
+        timer.scheduleAtFixedRate(new HomeActivity.SliderTimer(),4000,4000);
+
+
     }
 
     @OnClick(R.id.taranimCardView)
@@ -69,6 +111,27 @@ public class HomeActivity extends AppCompatActivity
         intent.putExtra("eventTypeName", eventName);
         startActivity(intent);
     }
+
+
+
+
+    class SliderTimer extends TimerTask {
+        @Override
+        public void run() {
+            HomeActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (viewPager.getCurrentItem()<pagers.size()-1)
+                    {
+                        viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
+                    }
+                    else
+                        viewPager.setCurrentItem(0);
+                }
+            });
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/example/vergionmaryapp/booking/showEvents/adapter/EventsListAdapter.kt
+++ app/src/main/java/com/example/vergionmaryapp/booking/showEvents/adapter/EventsListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vergionmaryapp.R
 import com.example.vergionmaryapp.models.booking.EventModule
@@ -13,28 +14,24 @@ import com.example.vergionmaryapp.utils.CommonMethod
 import kotlinx.android.synthetic.main.item_event_list.view.*
 
 
-class EventsListAdapter(private var eventsList : ArrayList<EventModule>,
+class EventsListAdapter(private var eventsList: ArrayList<EventModule>,
                         private var listener: IEventsClickListener,
-                        private var context : Context)
-    : RecyclerView.Adapter<EventsListAdapter.EventHolder>()
-{
+                        private var context: Context)
+    : RecyclerView.Adapter<EventsListAdapter.EventHolder>() {
     private var fade: Animation? = null
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventHolder
-    {
-        val  layoutInflater = LayoutInflater.from(parent.context)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
         val view: View = layoutInflater.inflate(R.layout.item_event_list, parent, false)
 
         return EventHolder(view)
     }
 
-    override fun getItemCount(): Int
-    {
+    override fun getItemCount(): Int {
         return eventsList.size
     }
 
-    override fun onBindViewHolder(holder: EventHolder, position: Int)
-    {
+    override fun onBindViewHolder(holder: EventHolder, position: Int) {
         fade = AnimationUtils.loadAnimation(context, R.anim.scale_animition)
         holder.myCardView.animation = fade
 
@@ -42,24 +39,40 @@ class EventsListAdapter(private var eventsList : ArrayList<EventModule>,
         val commonMethod = CommonMethod()
         var eventDate = ""
 
-        if(eventObject.eventDate != null && eventObject.eventDate != "00:00:00")
+        if (eventObject.eventDate != null && eventObject.eventDate != "00:00:00")
             eventDate = eventObject.eventDate?.let { commonMethod.separateString(it) }.toString()
 
         holder.eventTitleTv.text = eventObject.eventName
         holder.eventDateTv.text = eventDate
         holder.eventDayTv.text = commonMethod.getDayNameFromDate(eventDate)
-        holder.fromTimeTv.text = eventObject.startTime?.substring(0,5)?.let { commonMethod.convert24Hto12H(it) }
-        holder.toTimeTv.text = eventObject.endTime?.substring(0,5)?.let { commonMethod.convert24Hto12H(it) }
-        if(eventObject.notes != null)
+        holder.fromTimeTv.text = eventObject.startTime?.substring(0, 5)?.let { commonMethod.convert24Hto12H(it) }
+        holder.toTimeTv.text = eventObject.endTime?.substring(0, 5)?.let { commonMethod.convert24Hto12H(it) }
+        if (eventObject.notes != null)
             holder.noteTv.text = eventObject.notes
 
-        holder.myCardView.setOnClickListener {
-            eventObject.id?.let { listener.onItemClicked(it) }
-        }.toString()
+        var s = 5
+        var ss = 5
+
+        if (eventObject.noOfAvailableTickets == eventObject.noOfReservedTickets) {
+
+            holder.relative_UnEnable.visibility = View.VISIBLE
+        }
+
+            holder.myCardView.setOnClickListener {
+                //
+                if (eventObject.noOfAvailableTickets != eventObject.noOfReservedTickets) {
+                    eventObject.id?.let {
+                        listener.onItemClicked(it)
+                    }
+                }
+                else{
+                    Toast.makeText(context, "العدد ممتلىء!", Toast.LENGTH_LONG).show()
+                }
+
+            }.toString()
     }
 
-    inner class EventHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
-    {
+    inner class EventHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val eventDateTv = itemView.eventDateTv!!
         val eventDayTv = itemView.eventDayTv!!
         val eventTitleTv = itemView.eventTitleTv!!
@@ -67,5 +80,6 @@ class EventsListAdapter(private var eventsList : ArrayList<EventModule>,
         val toTimeTv = itemView.to_time!!
         val myCardView = itemView.iteme_event_linear!!
         val noteTv = itemView.noteTv!!
+        val relative_UnEnable = itemView.card_unenable!!
     }
 }
